59989832a95c26f81134695a72f62b6440f9f3e4
Ritesh <ritesh.rlb@gmail.com>
2022-09-17 11:25:09 +0530

    Carousel related changes

---
+++ RK.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".your-class").slick({
+    arrows: true,
+    centerMode: true,
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+});
